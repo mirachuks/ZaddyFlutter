8139c50216e5470e8bafde9c94ac8c23e9e31adf
nwalichukz <chukznwali@gmail.com>
2026-05-29 09:49:24 +0100

    ok

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Models\UserWallet;
 use JWTAuth, Validator;
 
 class AuthController extends Controller
